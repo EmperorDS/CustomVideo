8469442d8d50d92b12994c192b797290d41ca0ad
qianfeng <qianfeng@qianfengdeMac-mini-3.local>
2015-10-27 16:13:50 +0800

    Initial Commit

---
+++ CustomVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomVideo
+//
+//  Created by qianfeng on 15/10/27.
+//  Copyright (c) 2015年 KaiQiangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
